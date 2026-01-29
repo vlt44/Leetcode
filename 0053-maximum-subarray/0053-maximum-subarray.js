9595242b6a9302dbbdc15bc65f570aae3dfb3d0e
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-01-29 12:45:03 -0600

    0053-maximum-subarray .js 4 ms (23.02%) 63.6 MB (80.87%)

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let currMax = nums[0];
+    let max = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        currMax = Math.max(nums[i], currMax + nums[i]);
+        max = Math.max( max, currMax);
+    }
+
+    return max;
+};
