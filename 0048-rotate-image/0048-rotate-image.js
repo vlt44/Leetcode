3a44862947dc1fbf1edd96a69400e97de6ca8070
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-02-10 15:50:22 -0600

    0048-rotate-image .js 0 ms (100%) 53.5 MB (92.06%)

---
+++ 0048-rotate-image/0048-rotate-image.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i; j < matrix.length; j++) {
+            
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix.length/2; j++) {
+            
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[i][matrix.length -1 -j];
+            matrix[i][matrix.length -1 -j] = temp;
+        }
+    }
+
+    return matrix;
+};
