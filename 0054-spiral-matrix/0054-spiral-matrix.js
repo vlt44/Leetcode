5c78a9448b37fdd1162af91d5ce4342afaea6419
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-02-10 15:50:39 -0600

    0054-spiral-matrix .js 0 ms (100%) 54.1 MB (28.43%)

---
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    let nums = [];
+    let left = 0;
+    let top = 0;
+    let right = matrix[0].length - 1;
+    let bottom = matrix.length - 1;
+    let size = matrix.length * matrix[0].length;
+
+    while(nums.length < size) {
+        for (let i = left; i <= right && nums.length < size; i++) {
+            nums.push(matrix[top][i]);
+        }
+        top++;
+
+        for (let i = top; i <= bottom && nums.length < size; i++) {
+            nums.push(matrix[i][right]);
+        }
+        right--;
+
+        for (let i = right; i >= left && nums.length < size; i--) {
+            nums.push(matrix[bottom][i]);
+        }
+        bottom--;
+
+        for (let i = bottom; i >= top && nums.length < size; i--) {
+            nums.push(matrix[i][left]);
+        }
+        left++;
+    }
+
+    return nums;
+};
