92f39b44611b854dffc941681554300b9330246b
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-02-26 19:32:25 -0600

    0133-clone-graph .js 58 ms (23.69%) 57.8 MB (31.71%)

---
+++ 0133-clone-graph/0133-clone-graph.js
@@ -0,0 +1,31 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+var cloneGraph = function(node) {
+    let visited = {};
+
+    function dfs(node) {
+        if (!node) return node;
+        if (!!visited[node.val]) return visited[node.val]
+
+        let root = new Node(node.val);
+        visited[node.val] = root;
+
+        for (let neighbor of node.neighbors) {
+            root.neighbors.push(dfs(neighbor))
+        }
+
+        return root;
+    }
+
+    return dfs(node);
+};
