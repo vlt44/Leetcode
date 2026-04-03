c7116bb9f1953ec1c434d9b25c8ddfc97b600a04
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-04-03 15:38:35 -0500

    0300-longest-increasing-subsequence .js 100 ms (21.32%) 55.9 MB (43.32%)

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    let dp = new Array(nums.length).fill(1);
+
+    for (let i = 0; i <= nums.length; i++) {
+        for (let j = i;j >= 0; j--) {
+            if ( nums[i] > nums[j]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1)
+            }
+        }
+    }
+    return Math.max(...dp);
+};
