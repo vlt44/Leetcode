9f90d7c5aa7d9ac6a9ec8cd3ad668607bc1526e7
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-04-10 15:53:44 -0500

    0008-string-to-integer-atoi .js 1 ms (87.93%) 57.2 MB (62.88%)

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    let index = 0;
+    let isNeg = false;
+    let res = 0;
+
+    for (let i = index; i < s.length; i++) {
+        if (s[i] === " ") {
+            index++;
+        } else {
+            break;
+        }
+    }
+
+    if (s[index] == '-' || s[index] == '+') {
+        isNeg = s[index] == '-';
+        index++;
+    }
+
+    for (let i = index; i < s.length; i++) {
+        let num = s.charCodeAt(i) - 48;
+
+        if (num < 0 || num > 9) break;
+
+        res *= 10;
+        res += num;
+    }
+
+    if (isNeg) {
+        res = -res;
+    }
+
+    let min = -(2**31);
+    let max = 2**31-1;
+
+    let minima = Math.min(max, res);
+
+    return Math.max(minima, min);
+};
