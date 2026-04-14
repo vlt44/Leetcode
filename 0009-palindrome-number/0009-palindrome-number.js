d84af952e4d069372c443115f0e2654459ca271d
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-04-14 13:41:31 -0500

    0009-palindrome-number .js 5 ms (77.82%) 64.8 MB (36.76%)

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if ( x < 0) return false;
+
+  x = x.toString();
+
+  let left = 0;
+  let right = x.length - 1;
+
+  while (left < right) {
+    if (x[left] !== x[right]) {
+        return false;
+    }
+
+    left++;
+    right--;
+  }  
+
+  return true;
+};
