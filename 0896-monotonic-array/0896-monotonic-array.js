8753f09c86438f529b88ea52a8b2b6eda62fe2ce
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-04-20 13:12:05 -0500

    0896-monotonic-array .js 4 ms (36.66%) 67.1 MB (50.12%)

---
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    let increasing = true;
+    let decreasing = true;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+            increasing = false;
+        }
+        if (nums[i] < nums[i + 1]) {
+            decreasing = false;
+        }
+    }
+
+    return increasing || decreasing;
+};
