3b355951a50f3ee5a8c155449c1d86defccc4029
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-05-27 22:29:08 -0500

    0257-binary-tree-paths .js 0 ms (100%) 56.4 MB (12.81%)

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+
+    if (!root) return [];
+
+    let res = [];
+
+    function dfs(root, path) {
+        path.push(root.val);
+
+        if (!root.left && !root.right) {
+            res.push(path.join("->"));
+        }
+
+        if (root.left) dfs(root.left, path)
+        if (root.right) dfs(root.right, path);
+
+        path.pop();
+    }
+
+    dfs(root, []);
+    return res;
+};
