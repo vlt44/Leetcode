a34eca6936f00d5a434982782a41bbef0ea411e1
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-06-09 21:53:45 -0500

    1328-break-a-palindrome .js 0 ms (100%) 53.5 MB (73.68%)

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} palindrome
+ * @return {string}
+ */
+var breakPalindrome = function(palindrome) {
+    if (palindrome.length === 1) return "";
+
+    let arr = palindrome.split("");
+
+    for (let i = 0; i < Math.floor(arr.length/2); i++) {
+        if (arr[i] !== "a") {
+            arr[i] = "a";
+            return arr.join("");
+        }
+    }
+
+    arr[arr.length - 1] = "b";
+    return arr.join("");
+};
