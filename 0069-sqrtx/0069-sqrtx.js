6ba632cd404a1f14ff37630da91ac0ed03feddc9
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-06-13 20:44:21 -0500

    0069-sqrtx .js 27 ms (9.09%) 56.2 MB (38.6%)

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if (x <= 1) return x;
+
+    for (let i = 2; i <= x; i++) {
+        if (i * i === x) return i;
+        if (i * i > x) return i - 1;
+    }
+};
