f090748de8691fd0f403823e495f37445b5e3ae4
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-06-18 21:59:27 -0500

    0645-set-mismatch .js 9 ms (67.87%) 59.7 MB (60.1%)

---
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    let output = [];
+
+    for (let num of nums) {
+        num = Math.abs(num);
+
+        if (nums[num - 1] < 0) {
+            output.push(num);
+        } else {
+            nums[num - 1] *= -1;
+        }
+    } 
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            output.push(i + 1)
+
+            return output;
+        }
+    }
+};
