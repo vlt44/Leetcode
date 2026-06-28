a86ed4f568c9e4703b583e172e56ac8644e332f2
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-06-27 19:13:33 -0500

    0334-increasing-triplet-subsequence .js 4 ms (32.85%) 73.6 MB (87.08%)

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let first = Infinity;
+    let second = Infinity;
+
+    for (const num of nums) {
+        if (num <= first) {
+            first = num;
+        } else if (num <= second) {
+            second = num;
+        } else {
+            return true
+        }
+    }
+
+    return false;
+};
