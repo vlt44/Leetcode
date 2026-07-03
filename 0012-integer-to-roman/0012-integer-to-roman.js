d36d791aac437cf144e406ed9a0886c67fa67cdd
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-07-03 11:33:33 -0500

    0012-integer-to-roman .js 7 ms (43.95%) 63.7 MB (30.35%)

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    let values = [
+        [1000, "M"],
+        [900, "CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"]
+    ];
+
+    let result = "";
+
+    for (let [value, symbol] of values) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
+};
