b614265ac12465ce03a63e812954c96a6ed1a66e
vlt44 <57538298+vlt44@users.noreply.github.com>
2026-07-05 10:40:01 -0500

    0119-pascals-triangle-ii .js 0 ms (100%) 52.6 MB (93.64%)

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    let row = [1];
+
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = i - 1; j > 0; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+
+        row.push(1);
+    }
+
+    return row;
+};
